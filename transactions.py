015690c50bd449e41791fa22e4018d32647e94e1
rbuchthal25 <119367191+rbuchthal25@users.noreply.github.com>
2023-03-23 15:28:45 -0400

    added

---
+++ transactions.py
@@ -0,0 +1,16 @@
+import sqlite3
+import os
+
+class tracker():
+    # robin
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.createTable()
+    
+    def createTable():
+        return
+
+    
+
+    
+        
