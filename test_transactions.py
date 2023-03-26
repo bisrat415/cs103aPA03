b68b91f8fa15d74b06bef0c2a03e406575611ca1
talspec2 <99153309+talspec2@users.noreply.github.com>
2023-03-25 20:55:40 -0400

    small changes

---
+++ test_transactions.py
@@ -0,0 +1,12 @@
+''' testing based imports '''
+from transactions import Transactions
+import pytest
+
+# implemented by 
+def test_show_transactions():
+    ''' 
+    tests the show transactions method by 
+    adding a transaction, then printing 
+    all transactions
+    '''
+
