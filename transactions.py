fef7a11ef83be2bf46823ba7a7a4e03a92e87c2c
talspec2 <99153309+talspec2@users.noreply.github.com>
2023-03-25 20:29:06 -0400

    conformed dock to pylint when possible

--- transactions.py
+++ transactions.py
@@ -1,8 +1,9 @@
 ''' allows access to sqlite3 '''
 import sqlite3
-
+# edited to match pylint when possible - Tal Spector
 class Transactions():
     ''' class to handle all transactions in given data set '''
+   
     # implemented by robin
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
@@ -21,32 +22,40 @@ class Transactions():
 
     # implemented by robin
     def add_transaction(self, item_number, amount, category, date, description):
-        self.cur.execute("INSERT INTO transactions VALUES (NULL,?,?,?,?,?)", (item_number, amount, category, date, description))
+        ''' adds a transaction '''
+        self.cur.execute("INSERT INTO transactions VALUES (NULL,?,?,?,?,?)",
+                         (item_number, amount, category, date, description))
         self.conn.commit()
 
     # implemented by robin
     def delete_transaction(self, id):
+        ''' deletes a transaction '''
         self.cur.execute("DELETE FROM transactions WHERE id=?", (id,))
         self.conn.commit()
 
     # implemented by robin
     def summarize_by_date(self):
+        ''' summarizes transactions by date '''
         self.cur.execute("SELECT date, SUM(amount) FROM transactions GROUP BY date")
         return self.cur.fetchall()
 
     # implemented by robin
     def summarize_by_month(self):
-        self.cur.execute("SELECT strftime('%m', date) AS month, SUM(amount) FROM transactions GROUP BY month")
+        ''' summarizes transactions by month '''
+        self.cur.execute('''SELECT strftime('%m', date) AS month, 
+                            SUM(amount) FROM transactions GROUP BY month''')
         return self.cur.fetchall()
 
     # implemented by robin
     def summarize_by_year(self):
-        self.cur.execute("SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year")
+        ''' summarizes transactions by year '''
+        self.cur.execute('''SELECT strftime('%Y', date) AS year,
+                         SUM(amount) FROM transactions GROUP BY year''')
         return self.cur.fetchall()
 
     # implemented by robin
     def summarize_by_category(self):
+        ''' summarizes transactions by category '''
         self.cur.execute("SELECT category, SUM(amount) FROM transactions GROUP BY category")
         return self.cur.fetchall()
-
     
